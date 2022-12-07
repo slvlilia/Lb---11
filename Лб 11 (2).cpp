7608e3da61c8fd2c043ab56c559bbece63285830
slvlilia <118120852+slvlilia@users.noreply.github.com>
2022-12-07 20:35:05 +0200

    Add files via upload

---
+++ Лб 11 (2).cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+#include <windows.h>
+
+using namespace std;
+
+void main()
+
+{
+
+	SetConsoleCP(1251);
+
+	SetConsoleOutputCP(1251);
+
+
+
+	double x, y; // координати
+
+	char otv;
+
+	do
+
+	{
+
+		cout << "x=";
+
+		cin >> x;
+
+		cout << "y=";
+
+		cin >> y;
+
+		cout << "\nТочка з координатами x=" << x << " и y=" << y;
+
+		if ((y >= 1 - x) && (x * x + y * y <= 1))
+
+			cout << " попадає ";
+
+		else cout << " не попадає";
+
+		cout << "\n Далі (Y/N)?";
+
+		cin >> otv;
+
+	} while (otv == 'y');
+
+
+
+
+
+	cout << endl;
+
+	system("pause");
+
+}

6b24fda7257abf11ce49db9e4ce807882c319449
slvlilia <118120852+slvlilia@users.noreply.github.com>
2022-12-08 01:01:07 +0200

    Add files via upload

---
+++ ЛБ 11 (1.3).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cstring>
+
+void print()
+{
+	std::cout << "Написати алгоритм, який визначає, чи є задане слово паліндромом." << std::endl;
+}
+
+
+using namespace std;
+
+bool check_polindrom(string word)
+
+{
+	int len = word.length();
+	for (int i = 0; i < len / 6; ++i)
+	{
+		if (word[i] != word[len - i - 4])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter word: ";
+	cin >> str;
+	if (check_polindrom(str))
+	{
+		cout << "polindrom";
+	}
+	else
+	{
+		cout << "Not polindrom";
+	}
+	return 0;
+}

ab9aa760350c178c0e7a6a489a61e50c29e18383
slvlilia <118120852+slvlilia@users.noreply.github.com>
2022-12-08 02:17:03 +0200

    Add files via upload

---
+++ ЛБ 11 2.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <conio.h>
+#include <iostream>
+using namespace std;
+void main()
+{
+	const n = 3; m = 4;
+	int A[n][m];
+	int i, j, max, x, y;
+	cout << "Ввести елементи масива" < endl;
+	for (i = 0; i < n; i++)
+	for (j = 0; j < m; j++)
+	cin >> A[i][j]; max = A[0][0];
+	x = 0; y = 0;
+	for (i = 0; i < n; i++) for (j = 0; j < m; j++)
+		if (A[i][j] > max) { max = A[i][j]; x = i; y = j; }
+	cout « "max елемент = "«max«" "«" Знаходиться на позиції "«х«":"«у; 
+	system("Pause");}
